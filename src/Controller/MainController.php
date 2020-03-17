dc923871b4fa9d02140608b4645979c0040fb641
KnHFr <franck.valet.pro@gmail.com>
2020-03-17 11:18:22 +0100

    Manipulation of the title key in the MainController and in the main / index.html.twig view OK

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Service\Parameter;
+use App\Entity\Parameter\Title;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/main", name="main")
      */
-    public function index()
+    public function index(Parameter $parameter)
     {
+        $titleData = new Title();
+        $titleData->title = $parameter->get("title");
         return $this->render('main/index.html.twig', [
+            'title' => $titleData,
             'controller_name' => 'MainController',
         ]);
     }
