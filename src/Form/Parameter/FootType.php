377f182f98524f776e7e36cd68e170b2a9d3191c
KnHFr <franck.valet.pro@gmail.com>
2020-03-20 12:50:43 +0100

    footer v0.2 OK

--- src/Form/Parameter/FootType.php
+++ src/Form/Parameter/FootType.php
@@ -7,13 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 
 class FootType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('contact', TextType::class, [
+        $builder->add('contact', TextareaType::class, [
             'label' => 'Vos informations de Contact'
         ]);
         $builder->add('socialNetwork', TextType::class, [
